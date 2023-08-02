47e7f509c533377e1672dfd604b035074668bfd3
abhik2207 <107669163+abhik2207@users.noreply.github.com>
2023-08-02 14:07:46 +0530

    Update script.js
    updated the logic

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let hour = document.getElementById("hour");
-let min = document.getElementById("min");
-let sec = document.getElementById("sec");
+let hour = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
 
-function displayTime() {
+function displayTime(){
     let myDate = new Date();
 
     let hh = myDate.getHours();
     let mm = myDate.getMinutes();
     let ss = myDate.getSeconds();
 
-    let h_rotation = 30 * hh + mm / 2;
-    let m_rotation = 6 * min;
-    let s_rotation = 6 * ss;
+    let h_rotation = 30*hh + mm/2;
+    let m_rotation = 6*mm;
+    let s_rotation = 6*ss;
 
     hour.style.transform = `rotate(${h_rotation}deg)`;
     min.style.transform = `rotate(${m_rotation}deg)`;
